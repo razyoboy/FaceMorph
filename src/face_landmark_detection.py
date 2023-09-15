089e49304866968ed9c1656b08f86c186f44422c
razyoboy <razoreiei@gmail.com>
2023-09-15 15:28:22 +0700

    implement option to include borders and export images

--- src/face_landmark_detection.py
+++ src/face_landmark_detection.py
@@ -3,12 +3,14 @@ import dlib
 import numpy as np
 
 
-def facial_landmarks_detector(predictor_path: str, picture: str):
+def facial_landmarks_detector(predictor_path: str, picture: str, include_borders: bool):
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(predictor_path)
 
     print("Processing file: {}".format(picture))
     img = dlib.load_rgb_image(picture)
+
+    height, width, _ = img.shape
 
     detectors = detector(img, 1)
     print("Number of faces detected: {}".format(len(detectors)))
@@ -19,7 +21,22 @@ def facial_landmarks_detector(predictor_path: str, picture: str):
         for i in range(0, 68):
             points[i] = (shape.part(i).x, shape.part(i).y)
 
+        # Adding border points if include_border is true
+        if include_borders:
+            border_points = [
+                (0, 0),
+                (width // 2, 0),
+                (width - 1, 0),
+                (width - 1, height // 2),
+                (width - 1, height - 1),
+                (width // 2, height - 1),
+                (0, height - 1),
+                (0, height // 2)
+            ]
+            points = np.vstack([points, border_points])
+
         return points
+
 
 if __name__ == "__main__":
 
@@ -29,7 +46,7 @@ if __name__ == "__main__":
     ]
 
     predictor_path = 'model/shape_predictor_68_face_landmarks.dat'
-    res = facial_landmarks_detector(predictor_path, faces[0])
+    res = facial_landmarks_detector(predictor_path, faces[1], True)
 
     from scipy.spatial import Delaunay
     tri = Delaunay(res)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-﻿import numpy as np
+﻿import os.path
+
+import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
@@ -6,7 +8,7 @@ from scipy.spatial import Delaunay
 from face_landmark_detection import facial_landmarks_detector
 from face_morph import morph_triangle
 
-def main(predictor_path, filename1, filename2, alpha):
+def main(predictor_path, filename1, filename2, alpha, include_borders, export_image):
     # Read images
     img1 = cv2.imread(filename1)
     img2 = cv2.imread(filename2)
@@ -16,8 +18,8 @@ def main(predictor_path, filename1, filename2, alpha):
     img2 = np.float32(img2)
 
     # Read array of corresponding points
-    points1 = np.array(facial_landmarks_detector(predictor_path, filename1))
-    points2 = np.array(facial_landmarks_detector(predictor_path, filename2))
+    points1 = np.array(facial_landmarks_detector(predictor_path, filename1, include_borders))
+    points2 = np.array(facial_landmarks_detector(predictor_path, filename2, include_borders))
     points = (1 - alpha) * points1 + alpha * points2
 
     # Allocate space for final output
@@ -42,14 +44,22 @@ def main(predictor_path, filename1, filename2, alpha):
 
     # Display Result
     plt.imshow(cv2.cvtColor(np.uint8(img_morph), cv2.COLOR_BGR2RGB))
-    plt.title('Morphed Face')
     plt.axis('off')  # No axes for this plot
+
+    if export_image:
+        filename1_without_ext = os.path.splitext(os.path.basename(filename1))[0]
+        filename2_without_ext = os.path.splitext(os.path.basename(filename2))[0]
+        output_path = f'output/morphed_{filename1_without_ext}_{filename2_without_ext}_alpha_{alpha}.jpg'
+        plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
+
     plt.show()
 
 if __name__ == '__main__' :
     predictor_path = 'model/shape_predictor_68_face_landmarks.dat'
     filename1 = 'img/donald_trump.jpg'
     filename2 = 'img/hillary_clinton.jpg'
-    alpha = 1.0
+    alpha = 0.5
+    include_borders = True
+    export_image = False
 
-    main(predictor_path, filename1, filename2, alpha)
+    main(predictor_path, filename1, filename2, alpha, include_borders, export_image)
