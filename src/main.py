fbd264fd7f3664f17d3b8c0f85ab666e0f6dcde0
razyoboy <razoreiei@gmail.com>
2023-09-21 13:53:43 +0700

    fix image size bug

--- src/main.py
+++ src/main.py
@@ -14,19 +14,30 @@ def main(predictor_path, filename1, filename2, alpha, include_borders, export_im
     img1 = cv2.imread(filename1)
     img2 = cv2.imread(filename2)
 
+    # Resize img2 to match img1 dimensions
+    img2 = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
+
+    # Read array of corresponding points
+    points1 = np.array(facial_landmarks_detector(predictor_path, img1, include_borders))
+    points2 = np.array(facial_landmarks_detector(predictor_path, img2, include_borders))
+    
     # Convert Mat to float data type
     img1 = np.float32(img1)
     img2 = np.float32(img2)
+    
+    if points1 is None or points2 is None:
+        print("Failed to detect face in one or both images.")
+        return
 
-    # Read array of corresponding points
-    points1 = np.array(facial_landmarks_detector(predictor_path, filename1, include_borders))
-    points2 = np.array(facial_landmarks_detector(predictor_path, filename2, include_borders))
     points = (1 - alpha) * points1 + alpha * points2
 
     # Allocate space for final output
-    img_morph = np.zeros(img1.shape, dtype = img1.dtype)
+    img_morph = np.zeros(img1.shape, dtype=img1.dtype)
 
     tri = Delaunay(points)
+
+    # Allocate space for transparency mask
+    transparency_mask = np.zeros(img1.shape[0:2], dtype=np.float32)
 
     # Read triangles from tri.txt
     for simplex in tri.simplices:
@@ -41,31 +52,40 @@ def main(predictor_path, filename1, filename2, alpha, include_borders, export_im
         t = [points[x], points[y], points[z]]
 
         # Morph one triangle at a time.
-        morph_triangle(img1, img2, img_morph, t1, t2, t, alpha)
+        morph_triangle(img1, img2, img_morph, t1, t2, t, alpha, transparency_mask)
+
+    # Generate the 4-channel output image with transparency
+    img_morph_4channel = np.zeros((img1.shape[0], img1.shape[1], 4), dtype=np.uint8)
+    img_morph_4channel[..., :3] = np.uint8(img_morph)
+    img_morph_4channel[..., 3] = np.uint8(transparency_mask * 255)
 
     if display_results:
-        plt.imshow(cv2.cvtColor(np.uint8(img_morph), cv2.COLOR_BGR2RGB))
+        plt.imshow(cv2.cvtColor(np.uint8(img_morph_4channel), cv2.COLOR_BGRA2RGBA))
         plt.axis('off')  # No axes for this plot
 
     if export_image:
         filename1_without_ext = os.path.splitext(os.path.basename(filename1))[0]
         filename2_without_ext = os.path.splitext(os.path.basename(filename2))[0]
-        output_path = f'output/morphed_{filename1_without_ext}_{filename2_without_ext}_alpha_{alpha}.jpg'
-        plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
+        if include_borders:
+            output_path = f'output/morphed_{filename1_without_ext}_{filename2_without_ext}_alpha_{alpha}.png'
+        else:
+            output_path = f'output/morphed_{filename1_without_ext}_{filename2_without_ext}_alpha_{alpha}_mask_only.png'
+        plt.savefig(output_path, bbox_inches='tight', pad_inches=0, transparent=True, format='png')
 
     plt.show()
 
 if __name__ == '__main__':
-    predictor_path = 'model/shape_predictor_68_face_landmarks.dat'
+    predictor_path = 'model/shape_predictor_68_face_landmarks.dat'  # Dlib's Predictor Path
+    alpha = 0.5  # Alpha level (0.5 for a mix of both faces)
+    
+    include_borders = True  # Whether to include borders during calculations
+    export_image = True  # Whether to export morphed images
+    display_results = True  # Whether to display morphed images result via matplotlib
+    
+    BATCH_MORPH = True
     filename1 = 'img/donald_trump.jpg'
     filename2 = 'img/ted_cruz.jpg'
-    alpha = 0.5
-    include_borders = True
-    export_image = True
-    display_results = False
-
-    BATCH_MORPH = True
-    folder_path = 'img/Male'
+    folder_path = 'img/Caucasian/M'
 
     if BATCH_MORPH:
         files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
